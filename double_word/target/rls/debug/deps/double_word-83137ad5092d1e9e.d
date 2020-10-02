982a710735b0cbbc94281428dbb8b24f348be3d5
James Joy <james.c.joy@gmail.com>
2020-10-01 22:44:30 -0400

    Basic implementation of double word function from ch 2

---
+++ double_word/target/rls/debug/deps/double_word-83137ad5092d1e9e.d
@@ -0,0 +1,5 @@
+c:\Users\james\Documents\rust\rust_interfaces_and_implementations\double_word\target\rls\debug\deps\double_word-83137ad5092d1e9e.rmeta: double_word\src\lib.rs
+
+c:\Users\james\Documents\rust\rust_interfaces_and_implementations\double_word\target\rls\debug\deps\double_word-83137ad5092d1e9e.d: double_word\src\lib.rs
+
+double_word\src\lib.rs:
---
+++ double_word/target/rls/debug/deps/double_word-8bccbcb45723df0d.d
@@ -0,0 +1,5 @@
+c:\Users\james\Documents\rust\rust_interfaces_and_implementations\double_word\target\rls\debug\deps\double_word-8bccbcb45723df0d.rmeta: double_word\src\lib.rs
+
+c:\Users\james\Documents\rust\rust_interfaces_and_implementations\double_word\target\rls\debug\deps\double_word-8bccbcb45723df0d.d: double_word\src\lib.rs
+
+double_word\src\lib.rs:
